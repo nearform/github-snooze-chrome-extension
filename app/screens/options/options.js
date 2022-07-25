95a4eb77eeab3fb5d7b2a9150af985e96875a532
Davide Roffo <davide.roffo@nearform.com>
2022-07-25 11:31:11 +0200

    feat: boilerplace react and chrome extension

--- app/screens/options/options.js
+++
@@ -1,7 +0,0 @@
-const saveBtn = document.getElementById('save-btn')
-
-saveBtn.addEventListener('click', async () => {
-  const newGreeting = document.getElementById('input-text').value
-  chrome.storage.sync.set({ greeting: newGreeting })
-  console.log('Greeting message updated with: ' + newGreeting)
-})
---
+++ src/main/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+const root = document.querySelector('#root')
+
+ReactDOM.render(<App />, root)
