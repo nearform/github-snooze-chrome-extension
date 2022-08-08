a3263cdaac71e25eb2d230475a31733ec4c612d1
Davide Roffo <davide.roffo@nearform.com>
2022-08-08 13:59:17 +0200

    feat: cleaning async and path join

--- scripts/updateManifest.js
+++ scripts/updateManifest.js
@@ -1,16 +1,12 @@
 const { writeFileSync } = require('fs')
-const path = require('path')
 
-const MANIFEST_PATH = path.join(__dirname, '../src/manifest.json')
+console.log('UPDATING MANIFEST ...')
+const manifest = require('../src/manifest.json')
+const packageJson = require('../package.json')
 
-const start = async () => {
-  console.log('UPDATING MANIFEST ...')
-  const manifest = require(MANIFEST_PATH)
-  const packageJson = require('../package.json')
-
-  manifest.version = packageJson.version
-  await writeFileSync(MANIFEST_PATH, JSON.stringify(manifest, null, 2))
-  console.log('MANIFEST UPDATED!')
-}
-
-start()
+manifest.version = packageJson.version
+writeFileSync(
+  __dirname + '/../src/manifest.json',
+  JSON.stringify(manifest, null, 2)
+)
+console.log('MANIFEST UPDATED!')
