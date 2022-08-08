79630f6d37f9c9d6831a3bacd038ccbb48bb5ac6
Davide Roffo <davide.roffo@nearform.com>
2022-08-08 13:29:47 +0200

    feat: add var for manifest path

--- updateManifest.js
+++ updateManifest.js
@@ -1,12 +1,14 @@
 const { writeFile } = require('fs/promises')
 
+const MANIFEST_PATH = './src/manifest.json'
+
 const start = async () => {
   console.log('UPDATING MANIFEST ...')
-  const manifest = require('./src/manifest.json')
+  const manifest = require(MANIFEST_PATH)
   const packageJson = require('./package.json')
 
   manifest.version = packageJson.version
-  await writeFile('./src/manifest.json', JSON.stringify(manifest, null, 2))
+  await writeFile(MANIFEST_PATH, JSON.stringify(manifest, null, 2))
   console.log('MANIFEST UPDATED!')
 }
 
