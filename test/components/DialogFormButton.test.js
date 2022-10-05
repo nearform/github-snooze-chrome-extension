1c754081d0cea97317cbd8467a8a844cde8dc41d
Bruno Pascottini <bruno.pascottini@nearform.com>
2022-10-05 10:19:15 +0100

    test: create snooze input

--- test/components/DialogFormButton.test.js
+++ test/components/DialogFormButton.test.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { fireEvent } from '@testing-library/react'
 import { render } from '../renderer'
+import { parseDate } from 'chrono-node'
 import DialogFormButton from '../../src/components/DialogFormButton'
 
 const props = {
@@ -24,5 +26,25 @@ describe('DialogFormButton.js', () => {
     )
 
     expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('autocomplete component to support custom snooze intervals', async () => {
+    const { findByText, getByRole } = render(<DialogFormButton {...props} />)
+    const snoozeMinutes = 27
+    const chronoInput = `in ${snoozeMinutes} minutes`
+
+    const openDialogButton = await findByText(props.label)
+    fireEvent.click(openDialogButton)
+
+    const input = await getByRole('combobox')
+    fireEvent.change(input, {
+      target: { value: chronoInput }
+    })
+
+    const expectedScheduleMessage = `You will be notified on ${parseDate(
+      chronoInput
+    ).toLocaleString()}`
+
+    await findByText(expectedScheduleMessage)
   })
 })
