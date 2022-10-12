8ae896efc3eb3555da43adf70cf31137f3e78f15
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-10-12 18:08:36 +0200

    feat: zip

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
+import zipPack from "vite-plugin-zip-pack";
+
 import manifest from './manifest.json'
+import pkg from './package.json'
 
 export default defineConfig({
   plugins: [
     react(),
     crx({ manifest }),
+    ...(process.env.NODE_ENV === 'production' ? [zipPack({outDir: 'releases', outFileName: `${pkg.name}-${pkg.version}.zip`})] : [])
   ],
   define: {
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
