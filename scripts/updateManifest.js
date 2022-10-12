eb62a2dd1ad840273d7fbff2249388feb3bdd47a
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-10-12 18:31:20 +0200

    fix: update manifest command

--- scripts/updateManifest.js
+++ scripts/updateManifest.js
@@ -1,12 +1,12 @@
 const { writeFileSync } = require('fs')
 
 console.log('UPDATING MANIFEST ...')
-const manifest = require('../src/manifest.json')
+const manifest = require('../manifest.json')
 const packageJson = require('../package.json')
 
 manifest.version = packageJson.version
 writeFileSync(
-  __dirname + '/../src/manifest.json',
+  __dirname + '/../manifest.json',
   JSON.stringify(manifest, null, 2)
 )
 console.log('MANIFEST UPDATED!')
